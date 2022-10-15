ad9e75ffcefc57bcb01e11b062608fdf5a32f072
causey96bc <causey904.bc@gmail.com>
2022-10-15 11:51:45 -0400

    adding firebase and initial setup

--- pumpkinvoter/src/App.tsx
+++ pumpkinvoter/src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { firebaseConfig } from "../.config";
-function App() {
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
 
-  return <div className="App"></div>;
+function App() {
+  return <div></div>;
 }
 
 export default App;
---
+++ pumpkinvoter/src/FireBaseApp.ts
@@ -0,0 +1,5 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./../.config";
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
